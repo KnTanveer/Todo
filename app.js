81762ff18dd8e235eeca3c04eb603921f255903b
KnTanveer <kntanveer7@gmail.com>
2025-09-07 17:35:25 +0530

    pwa

--- app.js
+++ app.js
@@ -493,6 +493,14 @@ buttons.forEach(btn => {
   });
 });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(reg => console.log("Service worker registered:", reg))
+      .catch(err => console.error("SW registration failed:", err));
+  });
+}
+
 // ---- initialise ----
 populateProjectSelect();
 renderAll();
